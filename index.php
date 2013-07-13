0022fac2b668ba147b33f57b42ab716be6cc858a
Matt Rude <m@mattrude.com>
2013-07-13 16:57:40 -0500

    Update white spaces in index.php

--- index.php
+++ index.php
@@ -3,75 +3,78 @@
 $USERPWD = "matt@mattrude.com:master12";
 $HTTPHEADER = "ldpz0jdrsx43jcyswlkn037vstg4ena3";
  
-    // Init cURL
-    $curl = curl_init();
-    // Set target URL
-    curl_setopt($curl, CURLOPT_URL, "https://api.pingdom.com/api/2.0/checks");
-    // Set the desired HTTP method (GET is default, see the documentation for each request)
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
-    // Set user (email) and password
-    curl_setopt($curl, CURLOPT_USERPWD, $USERPWD);
-    // Add a http header containing the application key (see the Authentication section of this document)
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array("App-Key: $HTTPHEADER"));
-    // Ask cURL to return the result as a string
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+// Init cURL
+$curl = curl_init();
+// Set target URL
+curl_setopt($curl, CURLOPT_URL, "https://api.pingdom.com/api/2.0/checks");
+// Set the desired HTTP method (GET is default, see the documentation for each request)
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+// Set user (email) and password
+curl_setopt($curl, CURLOPT_USERPWD, $USERPWD);
+// Add a http header containing the application key (see the Authentication section of this document)
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("App-Key: $HTTPHEADER"));
+// Ask cURL to return the result as a string
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
  
-    // Init cURL
-    $rtime = curl_init();
-    // Set target URL
-    curl_setopt($rtime, CURLOPT_URL, "https://api.pingdom.com/api/2.0/servertime");
-    // Set the desired HTTP method (GET is default, see the documentation for each request)
-    curl_setopt($rtime, CURLOPT_CUSTOMREQUEST, "GET");
-    // Set user (email) and password
-    curl_setopt($rtime, CURLOPT_USERPWD, $USERPWD);
-    // Add a http header containing the application key (see the Authentication section of this document)
-    curl_setopt($rtime, CURLOPT_HTTPHEADER, array("App-Key: $HTTPHEADER"));
-    // Ask cURL to return the result as a string
-    curl_setopt($rtime, CURLOPT_RETURNTRANSFER, 1);
+// Init cURL
+$rtime = curl_init();
+// Set target URL
+curl_setopt($rtime, CURLOPT_URL, "https://api.pingdom.com/api/2.0/servertime");
+// Set the desired HTTP method (GET is default, see the documentation for each request)
+curl_setopt($rtime, CURLOPT_CUSTOMREQUEST, "GET");
+// Set user (email) and password
+curl_setopt($rtime, CURLOPT_USERPWD, $USERPWD);
+// Add a http header containing the application key (see the Authentication section of this document)
+curl_setopt($rtime, CURLOPT_HTTPHEADER, array("App-Key: $HTTPHEADER"));
+// Ask cURL to return the result as a string
+curl_setopt($rtime, CURLOPT_RETURNTRANSFER, 1);
 
 
-    // Execute the request and decode the json result into an associative array
-    $response = json_decode(curl_exec($curl),true);
-    $time = json_decode(curl_exec($rtime),true);
+// Execute the request and decode the json result into an associative array
+$response = json_decode(curl_exec($curl),true);
+$time = json_decode(curl_exec($rtime),true);
  
-    // Check for errors returned by the API
-    if (isset($response['error'])) {
-        print "Error: " . $response['error']['errormessage'] . "\n";
-        exit;
-    }
+// Check for errors returned by the API
+if (isset($response['error'])) {
+    print "Error: " . $response['error']['errormessage'] . "\n";
+    exit;
+}
  
-    echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Public Website Health Status for Automattic - Sites</title>
-        <link rel="stylesheet" type="text/css" href="./style.css">
-    </head>
-    <body>
-        <div id="container">
-            <div id="section_curr_status">';
-
-    echo '<div class="date floatright" id="psp_last_update">';
-    print gmdate("M, j Y H:i:s e", $time['servertime']);
-    echo '</div>';
-    echo '<h2>Current Performance and Availability Status</h2>
-    <table width="100%" border="0" cellpadding="0" cellspacing="0" class="psp-table" id="table_curr_status"><tbody>
-    <tr>
-        <td width="28%" class="tablehead" colspan="2">Service / Website</td>
-        <td width="34%" class="tablehead">Performance and Availability Status</td>
-        <td width="23%" class="tablehead">Current Performance</td>
-        <td width="15%" class="tablehead">Uptime Last 24h</td>
-    </tr>';
-    // Fetch the list of checks from the response
-    $checksList = $response['checks'];
-    // Print the names and statuses of all checks in the list
-    foreach ($checksList as $check) {
-        print "<tr>";
-        print "<td><img src=\"./images/" . $check['status'] . "-status.png\"></td>";
-        print "<td>" . $check['name'] . "</td>";
-        print "<td>" . $check['status'] . "</td>";
-        print "<td>" . $check['lastresponsetime'] . "ms</td>";
-        print "<td>" . gmdate("Y-m-d\TH:i:s\Z", $check['lasterrortime']) . "</tr>";
-        print "</td>";
-    }
-    echo '</tbody></table></div>';
-    echo '</div></body>'; ?>
+echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Public Website Health Status for Automattic - Sites</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+    <div id="container">
+        <div id="section_curr_status">
+            <div class="date floatright" id="psp_last_update">';
+                print gmdate("M, j Y H:i:s e", $time['servertime']);echo '
+            </div>
+            <h2>Current Performance and Availability Status</h2>
+            <table width="100%" border="0" cellpadding="0" cellspacing="0" class="psp-table" id="table_curr_status">
+                <tbody>
+                    <tr>
+                        <td width="28%" class="tablehead" colspan="2">Service / Website</td>
+                        <td width="34%" class="tablehead">Performance and Availability Status</td>
+                        <td width="23%" class="tablehead">Current Performance</td>
+                        <td width="15%" class="tablehead">Uptime Last 24h</td>
+                    </tr>';
+                    // Fetch the list of checks from the response
+                    $checksList = $response['checks'];
+                    // Print the names and statuses of all checks in the list
+                    foreach ($checksList as $check) {
+                        print "<tr>";
+                        print "<td><img src=\"./images/" . $check['status'] . "-status.png\"></td>";
+                        print "<td>" . $check['name'] . "</td>";
+                        print "<td>" . $check['status'] . "</td>";
+                        print "<td>" . $check['lastresponsetime'] . "ms</td>";
+                        print "<td>" . gmdate("Y-m-d\TH:i:s\Z", $check['lasterrortime']) . "</tr>";
+                        print "</td>";
+                    }
+                echo '</tbody>
+            </table>
+        </div>
+    </div>
+</body>'; ?>
